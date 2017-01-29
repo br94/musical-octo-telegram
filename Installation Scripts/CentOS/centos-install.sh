983f6040e36d1c1947d78541baab9188fb9f9ab3
br94 <bryan.reussner@gmx.de>
2017-01-29 18:34:16 +0100

    Update centos-install.sh

--- Installation Scripts/CentOS/centos-install.sh
+++ Installation Scripts/CentOS/centos-install.sh
@@ -8,11 +8,11 @@ yum update -yy
 
 # Adds Nux Repos to fix Fontrendering
 echo '(2/18) Installing epel-release repository...'
-yum -y install epel-release && rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+yum -y install epel-release 
+yum -y install http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
 
 echo '(3/18) Adds Nux Repos to fix Fontrendering...'
 yum makecache fast
-yum clean all
 
 # Echoes font config
 echo '(4/18) Create font config...'
@@ -56,15 +56,13 @@ chown -v centos ~/.fonts.conf
 
 #Enables Font Fixes
 echo '(6/18) Enable font fixes...'
-yum --enablerepo=nux-dextop install fontconfig-infinality
-yum --enablerepo=nux-dextop install cairo
-yum --enablerepo=nux-dextop install libXft
-yum --enablerepo=nux-dextop install freetype-infinality
+yum --enablerepo=nux-dextop install -yy fontconfig-infinality
+yum --enablerepo=nux-dextop install -yy cairo
+yum --enablerepo=nux-dextop install -yy libXft
+yum --enablerepo=nux-dextop install -yy freetype-infinality
 
 echo '(7/18) Installing additional Icon Themes...'
-yum --enablerepo=nux-dextop install faience-icon-theme
-
-yum clean all
+yum --enablerepo=nux-dextop install -yy faience-icon-theme
 
 #Needed to Work with NTFS USB Drives
 echo '(8/18) Installing ntfs packages...'
@@ -89,8 +87,6 @@ yum-config-manager \
 yum makecache fast
 yum install -yy docker-engine
 
-yum clean all
-
 #Create usergroup to access docker without sudo
 echo '(11/18) Granting $USER right to user docker...'
 groupadd docker
@@ -105,22 +101,25 @@ yum install -yy git
 echo '(14/18) Installing svn...'
 yum install -yy svn
 
-#Downloads Java from the oracle page
+#Downloading and Installing Eclipse for Java EE
 echo '(15/18) Downloading and installing Eclipse IDE...'
-wget "http://www.mirrorservice.org/sites/download.eclipse.org/eclipseMirror/technology/epp/downloads/release/neon/2/eclipse-jee-neon-2-linux-gtk-x86_64.tar.gz"
-mkdir Eclipse
-cd Eclipse/
+wget "http://www.mirrorservice.org/sites/download.eclipse.org/eclipseMirror/technology/epp/downloads/release/neon/2/eclipse-jee-neon-2-linux-gtk-x86_64.tar.gz" -P ~/
+echo 'Download completed.. Starting installation...'
+mkdir ~/Eclipse
+cd ~/Eclipse/
 mv ~/eclipse-jee-neon-2-linux-gtk-x86_64.tar.gz ~/Eclipse
 tar xf eclipse-jee-neon-2-linux-gtk-x86_64.tar.gz
 chown -R $USER:$USER eclipse-jee-neon-2-linux-gtk-x86_64
-cd ~
 rm eclipse-jee-neon-2-linux-gtk-x86_64
+cd ~
 
 #Installing Apache Maven
 echo '(16/18) Installing maven...'
 yum install -yy maven
 
 #Installing Skype for Linux ALPHA
+echo '(17/18) Downloading and installing skype f
+
 echo '(17/18) Downloading and installing skype for linux alpha...'
 wget "https://go.skype.com/skypeforlinux-64-alpha.rpm"
 yum localinstall -yy skypeforlinux-64-alpha.rpm
